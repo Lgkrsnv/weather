2f329715bcfbac61472a89d9220dabfb8c053799
Olga <olga.dibts@ya.ru>
2021-06-27 22:13:29 +0300

    back

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const createError = require('http-errors');
+const logger = require('morgan');
+const path = require('path');
+
+
+const indexRouter = require('./routes/index');
+
+const { model } = require('mongoose');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', indexRouter);
+
+
+app.use((req, res, next) => {
+  const error = createError(404, 'Запрашиваемой страницы не существует на сервере.');
+  next(error);
+});
+
+
+app.use(function (err, req, res, next) {
+  // Получаем текущий ражим работы приложения.
+  const appMode = req.app.get('env');
+  // Создаём объект, в котором будет храниться ошибка.
+  let error;
+
+  // Если мы находимся в режиме разработки, то отправим в ответе настоящую ошибку. В противно случае отправим пустой объект.
+  if (appMode === 'development') {
+    error = err;
+  } else {
+    error = {};
+  }
+
+  // Записываем информацию об ошибке и сам объект ошибки в специальные переменные, доступные на сервере глобально, но только в рамках одного HTTP-запроса.
+  res.locals.message = err.message;
+  res.locals.error = error;
+
+  // Задаём в будущем ответе статус ошибки. Берём его из объекта ошибки, если он там есть. В противно случае записываем универсальный стату ошибки на сервере - 500.
+  res.status(err.status || 500);
+  // Формируем HTML-текст из шаблона "error.hbs" и отправляем его на клиент в качестве ответа.
+  res.render('error');
+});
+
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const app = require('./app');
+const mongoose = require("mongoose");
+
+const port = process.env.PORT ?? 5500;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}.....`);
+
+  mongoose.connect('mongodb://localhost:27017/weather', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }, () => {
+    console.log('Connection to weather database is successful.');
+  });
+})
